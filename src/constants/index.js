ef5190ef6936f2f19e46e411016140f00e5dc99b
Noe Cruz <educacion.mcon88@gmail.com>
2023-12-27 00:13:39 -0600

    fix (technologies): modifying know skills

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,28 +73,12 @@ const technologies = [
         icon: javascript,
     },
     {
-        name: 'TypeScript',
-        icon: typescript,
-    },
-    {
         name: 'React JS',
         icon: reactjs,
     },
     {
-        name: 'Redux Toolkit',
-        icon: redux,
-    },
-    {
         name: 'Tailwind CSS',
         icon: tailwind,
-    },
-    {
-        name: 'Node JS',
-        icon: nodejs,
-    },
-    {
-        name: 'MongoDB',
-        icon: mongodb,
     },
     {
         name: 'Three JS',
@@ -107,10 +91,6 @@ const technologies = [
     {
         name: 'figma',
         icon: figma,
-    },
-    {
-        name: 'docker',
-        icon: docker,
     },
 ]
 
